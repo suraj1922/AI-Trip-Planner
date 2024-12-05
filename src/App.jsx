7f132421600a0ac550f6feb4a6f669b1d1164511
SurajGupta <surajgupta19224@gmail.com>
2024-12-05 12:15:42 +0530

    "Removed Welcome header and Button from App component, replaced with Hero component. Added Header component to main.jsx"

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,7 @@ function App() {
 
   return (
     <>
-      <h2>Welcome To Trip Planner</h2>
-      <Button>Subscribe</Button>
+      <Hero />
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -16,6 +16,7 @@ const router=createBrowserRouter([
 ])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Header />
     <RouterProvider  router={router}/>
   </StrictMode>
 )
