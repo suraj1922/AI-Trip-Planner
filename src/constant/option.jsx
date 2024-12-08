d07b1ffc0a3729b39dd305a5aa384fd6a370032f
SurajGupta <surajgupta19224@gmail.com>
2024-12-08 20:54:43 +0530

    Working on data to be shown in the home page

---
+++ src/constant/option.jsx
@@ -0,0 +1,24 @@
+export const SelectTravelesList = [
+    {
+        id:1,
+        title:'Just Me',
+        desc: 'A sole travelesin exploration',
+        icons:'',
+        people:1,
+    },
+    {
+        id:2,
+        title:'A Couple',
+        desc: 'Two Traveles in tandem',
+        icons:'',
+        people:2,
+    },
+    {
+        id:3,
+        title:'Family',
+        desc:'A group pf fun loving adv',
+        icons:'',
+        people: '3 to 4 People',
+    },
+]
+
