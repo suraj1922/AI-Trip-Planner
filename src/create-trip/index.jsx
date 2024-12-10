5144761730ebeffc24ba08cd42cc8b65ddf10da3
SurajGupta <surajgupta19224@gmail.com>
2024-12-10 17:08:27 +0530

    `Refactored JSX code in create-trip/index.jsx to remove unnecessary whitespace and correct formatting`

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -35,25 +35,25 @@ function CreateTrip() {
       <div>
         <h2 className='text-xl my-3 font-medium'>What is Your Budget?</h2>
         <div className='grid grid-cols-3 gap-5 mt-5'>
-          {SelectBudgetOptions.map((item, index) => {
+          {SelectBudgetOptions.map((item, index) => (
             <div key={index} className='p-4 border cursor-pointer rounded-lg hover:shadow-lg'>
               <h2 className='text-4xl'>{item.icons}</h2>
               <h2 className='font-bold'>{item.title}</h2>
               <h2 className='text-sm text-gray-500'>{item.desc}</h2>
             </div>
-          })}
+          ))}
         </div>
       </div>
       <div>
         <h2 className='text-xl my-3 font-medium'>Who do you plan to travlling with on your next adventure</h2>
         <div className='grid grid-cols-3 gap-5 mt-5'>
-          {SelectTravelesList.map((item, index) => {
+          {SelectTravelesList.map((item, index) => (
             <div key={index} className='p-4 border cursor-pointer rounded-lg hover:shadow-lg'>
               <h2 className='text-4xl'>{item.icons}</h2>
               <h2 className='font-bold'>{item.title}</h2>
               <h2 className='text-sm text-gray-500'>{item.desc}</h2>
             </div>
-          })}
+          ))}
         </div>
       </div>
       
